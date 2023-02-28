f46a07f4b165bfd05389b64479c5e7d7f709e3e0
XpIose <raiunwes@gmail.com>
2023-02-28 04:09:41 -0700

    find component built out, frontend ready for backend

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 export const Find = ({user, setGreeting, setBtnColor}) => {
+
   useEffect(() => {
     setGreeting(`Welcome, ${user}`)
     setBtnColor('grey');
   }, [user]);
+
   return (
-    <div>
-      find
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequuntur recusandae, officia corrupti ad, ut laboriosam est fuga veritatis odit minus aliquid iure modi architecto. Tenetur deleniti explicabo unde voluptates.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequuntur recusandae, officia corrupti ad, ut laboriosam est fuga veritatis odit minus aliquid iure modi architecto. Tenetur deleniti explicabo unde voluptates.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequuntur recusandae, officia corrupti ad, ut laboriosam est fuga veritatis odit minus aliquid iure modi architecto. Tenetur deleniti explicabo unde voluptates.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequuntur recusandae, officia corrupti ad, ut laboriosam est fuga veritatis odit minus aliquid iure modi architecto. Tenetur deleniti explicabo unde voluptates.
-   
-      {/* <div id="header_find">
+    <div className='find'>
+      <div className='findSettings'>
+        <div className='searchInput'>R</div>
+        <div className='searchInput'>C</div>
+        <div className='searchInput'>$</div>
+      </div>
+      <div className='findResult'>
+        <div className='resultArea'>You can't out pizza the hut</div>
+        <div className='response'>
+          <div className='res'>X</div>
+          <div className='res'>./</div>
+          <div className='res'>-></div>
+        </div>
+        
+        <p className='resultInfo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, perspiciatis tempore quisquam hic reiciendis nihil commodi nisi necessitatibus deserunt. Dolorem consequatur fugit.
+        </p>
+        </div>
+    </div>
+  );
+};
+
+      /* <div id="header_find">
         <div id="header_find_caption">
           <p>HELLOWWW</p>
         </div>
@@ -39,8 +55,4 @@ export const Find = ({user, setGreeting, setBtnColor}) => {
         <div id="find_buttons">
 
         </div>
-      </div> */}
-
-    </div>
-  )
-}
+      </div> */
