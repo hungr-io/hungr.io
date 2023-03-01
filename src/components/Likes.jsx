1d8dadb67cc5532a888747a1ac0f02b332acac77
Rylan Wessel <107288347+XpIose@users.noreply.github.com>
2023-02-28 19:48:49 -0700

    Rylan/frontend (#3)
    * navbar, nio, likes, and find routes
    
    * find component built out, frontend ready for backend
    
    * fixed nav bar
    
    * fetch req to backend added

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,30 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import reactIcon from '/src/assets/react.svg'
 
+
 export const Likes = ({user, setGreeting}) => {
   
-  const [likes, setLikes] = useState([{name: 'McDonalds', price: '$', img: reactIcon}, 
-  {name: 'Pizza Hut', price: '$$', img: reactIcon},
-  {name: 'Sushi Sen', price: '$$$$', img: reactIcon},
-]);
+  const [likes, setLikes] = useState([]);
 
-  console.log(likes) 
+  
   useEffect(() => {
     setGreeting(`${user}'s Favorites`);
-    // getLikes();
+    getLikes();
+    // console.log(likes) 
   }, [user]);
 
   const getLikes = () => {
     console.log('getting likes...')
     //fetch likes
-    //set likes state
-    // setLikes([{name: 'McDonalds', price: '$$$'}])
+    fetch('api/getLikes')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setLikes(data);
+    })
+    .catch(err => console.log(err))
+    //set likes state for dev
+    setLikes([{name: 'McDonalds', price: '$', img: reactIcon}, 
+      {name: 'Pizza Hut', price: '$$', img: reactIcon},
+      {name: 'Sushi Sen', price: '$$$$', img: reactIcon},
+    ]);
+  }
+
+  const deleteFav = (id) => {
+    console.log('deleting fav by id...')
+    fetch('api/deleteFav', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setLikes(data);
+    })
   }
 
   return (
     <div className='likes'>
       {likes.map((el, i) => (
-        <div key={i} className='likesContainer'>
+        <div key={i} id={el._id} className='likesContainer'>
           <div className='likesImg'>
             <img src={el.img}></img>
           </div>
@@ -37,7 +62,7 @@ export const Likes = ({user, setGreeting}) => {
             </div>
           </div>
           <div >
-            <button className='likesDelBtn'>Delete</button>
+            <button className='likesDelBtn' onClick={() => deleteFav(el._id)}>Delete</button>
           </div>
         </div>
       ))}
