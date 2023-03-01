047523558966d319daf73c123e6d54f405e97251
Bianca Hua <biancahua@gmail.com>
2023-03-01 10:43:32 -0800

    fixed merge issue

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useHistory } from 'react';
-import { useNavigate, Link, useLocation  } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
-import { GoogleLogin } from '@react-oauth/google';
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
-import { Home } from './Home.jsx'
-
+import { Home } from "./Home.jsx";
+import "../style/login.scss";
 
 export const Login = (props) => {
   const { user, setUser, selectedPage, setSelectedPage } = props;
@@ -52,27 +52,44 @@ export const Login = (props) => {
 
 
   return (
-    <div>    
-      
-        <form id="login-form" className="form" onSubmit={(e) => handleLogin(e, 'manual', { 'email': document.getElementById("login-username").value, 'password': document.getElementById("login-password").value})}>
-            <h1>Welcome back</h1>
-            <div className="form-control-login">   
-                <input type="text" id="login-username" className="form-input-login" placeholder="Email" />              
-            </div>
-            <div className="form-control-login">             
-                <input type="password" id="login-password" className="form-input-login" placeholder="Password" />               
-            </div>
-            <div className="form-control-signin">
-              <button type="submit" className="form-button" >Log in</button>
-                </div>
-        </form>
-
-        <div>
-          Don't have an account yet? <Link to='/signup'>Register Now</Link>
+    <div className="login-page">
+      <div className="login-logo"></div>
+      <form
+        id="login-form"
+        className="form"
+        onSubmit={(e) =>
+          handleLogin(e, "manual", {
+            email: document.getElementById("login-username").value,
+            password: document.getElementById("login-password").value,
+          })
+        }
+      >
+        <h1>Welcome back</h1>
+        <div className="form-control-login">
+          <input
+            type="text"
+            id="login-username"
+            className="form-input-login"
+            placeholder="Email"
+          />
         </div>
-      
+        <div className="form-control-login">
+          <input
+            type="password"
+            id="login-password"
+            className="form-input-login"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-control-login">
+          <button type="submit" className="form-button">
+            Log in
+          </button>
+        </div>
+      </form>
+
       <div className="app_signup_google">
-        <GoogleLogin 
+        <GoogleLogin
           buttonText="Login with Google"
           onSuccess={async (credResponse) => {
             var decoded = await jwt_decode(credResponse.credential);
@@ -82,14 +99,13 @@ export const Login = (props) => {
           cookiePolicy="single_host_origin"          
         />
       </div>
-    <div>loginnn
-      <Link to='/home'>temp</Link>
+      <div className="signup-link">
+        Don't have an account yet? <Link to="/signup">Register Now</Link>
+      </div>
+      {/* <div>
+        loginnn
+        <Link to="/home">temp</Link>
+      </div> */}
     </div>
-  </div>
-  )
-}
-
-
-
-
-
+  );
+};
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,38 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
-
-
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../style/login.scss";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 export const Signup = () => {
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (window.google) {
-  //     google.accounts.id.initialize({
-  //       client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-  //       callback: handleGoogle,
-  //     });
+  const handleSignup = async (e, loginMethod, user) => {
+    if (loginMethod === "manual") {
+      e.preventDefault();
+      createUser();
+      navigate("/home");
+    } else {
+      console.log("this is my data: ", user);
+      const isExist = verifyUser();
+      if (isExist) {
+        navigate("/home");
+      } else {
+        createUser();
+      }
+    }
 
-  //     google.accounts.id.renderButton(document.getElementById(''));
-  //   }
-  // })
-
+    const createUser = async () => {
+      try {
+        const res = await fetch("/api/signup", {
+          method: "POST",
+          body: JSON.stringify(user),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    const verifyUser = async () => {
+      try {
+        const res = await fetch("/api/login", {
+          method: "POST",
+          body: JSON.stringify(user),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  };
   return (
-    <div id='signup'>
-       <form id="signup-form" className="form" >
-            <h1>Sign Up</h1>
-            <div className="form-control-signup">   
-                <input type="text" id="signup-username" className="form-input-signup" placeholder="Username" />              
-            </div>
-            <div className="form-control-signup">   
-                <input type="text" id="signup-username" className="form-input-signup" placeholder="Email" />              
-            </div>
-            <div className="form-control-signup">             
-                <input type="password" id="signup-password" className="form-input-signup" placeholder="Password" />               
-            </div>
-            <div className="form-control-signup">
-              <button type="submit" className="form-button"  >Sign up</button>
-                </div>
-        </form>
+    <div className="signup-page">
+      <form
+        id="login-form"
+        className="form"
+        onSubmit={(e) =>
+          handleSignup(e, "manual", {
+            email: document.getElementById("login-username").value,
+            password: document.getElementById("login-password").value,
+          })
+        }
+      >
+        <h1>Sign Up</h1>
+        <div className="form-control-login">
+          <input
+            type="text"
+            id="signup-username"
+            className="form-input-signup"
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-control-login">
+          <input
+            type="text"
+            id="signup-username"
+            className="form-input-signup"
+            placeholder="Email"
+          />
+        </div>
+        <div className="form-control-login">
+          <input
+            type="password"
+            id="signup-password"
+            className="form-input-signup"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-control-signup">
+          <button type="submit" className="form-button">
+            Sign up
+          </button>
+        </div>
+      </form>
+      <div className="app_signup_google">
+        <GoogleLogin
+          buttonText="Register with Google"
+          onSuccess={async (credResponse) => {
+            var decoded = await jwt_decode(credResponse.credential);
+            handleSignup(null, "google", {
+              email: decoded.email,
+              password: decoded.password,
+            });
+          }}
+          onFailure={() => console.log("login failed")}
+          cookiePolicy="single_host_origin"
+        />
+      </div>
+      <div className="signup-link">
+        Already have login and password? <Link to="/signup">Log in here</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
